890cd3f977369bef071626f951e5bd34ee5b16aa
MarcBonamusa <marc.bonamusa.7e6@itb.cat>
2026-01-28 20:01:50 +0100

    Carrito va

--- src/components/CurrentBuildWidget.vue
+++ src/components/CurrentBuildWidget.vue
@@ -6,26 +6,24 @@ const buildStore = useBuildStore();
 </script>
 
 <template>
-  <div class="cart-container">
-    <h1>Cistella de la compra</h1>
-    
-    <div v-if="buildStore.components.length === 0">
-      <p>La cistella esta buida.</p>
+    <div class="cart-container">
+        <h1>Cistella de la compra</h1>
+
+        <div v-if="buildStore.components.length === 0">
+            <p>La cistella esta buida.</p>
+        </div>
+
+        <ul v-else>
+            <li v-for="(items, type) in buildStore.groupedByType" :key="type">
+                <strong>{{ type }}:</strong> {{ items.length }}x {{ items[0].name }} - {{ items[0].price * items.length }}€
+
+                <button @click="buildStore.removeComponent(items[0].name)">
+                    Eliminar
+                </button>
+            </li>
+        </ul>
+
+        <hr />
+        <h3>Total: {{ buildStore.totalPrice }}€</h3>
     </div>
-
-    <ul v-else>
-      <li v-for="(items, type) in buildStore.groupedByType" :key="type">
-        <strong>{{ type }}:</strong> 
-        {{ items.length }}x {{ items[0].name }} 
-        - {{ items[0].price * items.length }}€
-        
-        <button @click="buildStore.removeComponent(items[0].name)">
-          Eliminar
-        </button>
-      </li>
-    </ul>
-
-    <hr />
-    <h3>Total: {{ buildStore.totalPrice }}€</h3>
-  </div>
 </template>
