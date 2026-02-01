dc01e3f43e0577ea4b0838734cd2e803f07e140c
MarcBonamusa <marc.bonamusa.7e6@itb.cat>
2026-02-01 11:39:46 +0100

    Commit final

--- src/stores/useUserSessionStore.js
+++ src/stores/useUserSessionStore.js
@@ -2,9 +2,9 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useUserSessionStore = defineStore('UserSessionStore', () => {
-
+    // State
     const userName = ref("Guest Builder")
-    
+    // Actions
     const visitGitHub = () => {
         const githubUser = 'MarcBonamusa'
         window.open(`https://github.com/${githubUser}`, '_blank')
